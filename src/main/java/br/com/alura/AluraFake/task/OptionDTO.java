58feac14d394812c37ade1b1fe9c09fb198e0eee
Alvarezpro87 <alvareznuvem@gmail.com>
2025-04-26 17:53:04 -0300

    feat(dto): add OptionDTO

---
+++ src/main/java/br/com/alura/AluraFake/task/OptionDTO.java
@@ -0,0 +1,11 @@
+package br.com.alura.AluraFake.task;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record OptionDTO(
+        @NotBlank
+        @Size(min = 4, max = 80)
+        String option,
+        boolean isCorrect
+) {}
