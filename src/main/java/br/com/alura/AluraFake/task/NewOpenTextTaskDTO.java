55023634ed5a1b1fd1d966eefc6bf62c75060cd4
Alvarezpro87 <alvareznuvem@gmail.com>
2025-04-26 18:01:25 -0300

    feat(dto): add DTO for open text activity

---
+++ src/main/java/br/com/alura/AluraFake/task/NewOpenTextTaskDTO.java
@@ -0,0 +1,9 @@
+package br.com.alura.AluraFake.task;
+
+import jakarta.validation.constraints.*;
+
+public record NewOpenTextTaskDTO(
+        @NotNull Long courseId,
+        @NotBlank @Size(min = 4, max = 255) String statement,
+        @Positive Integer order
+) {}
