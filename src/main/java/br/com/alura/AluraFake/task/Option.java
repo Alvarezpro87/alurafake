d226b94152760dc15d54427ee6114abd245d5b15
Alvarezpro87 <alvareznuvem@gmail.com>
2025-04-26 16:38:20 -0300

    feat(task): create Option entity (choice alternatives)

---
+++ src/main/java/br/com/alura/AluraFake/task/Option.java
@@ -0,0 +1,30 @@
+package br.com.alura.AluraFake.task;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+
+
+@Entity
+@Table(name = "options")
+public class Option {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 4, max = 80)
+    @Column(nullable = false, length = 80)
+    private String option;
+
+    private boolean isCorrect;
+
+    @ManyToOne(optional = false)
+    private Task task;
+
+    protected Option() { }
+
+    public Option(String option, boolean isCorrect, Task task) {
+        this.option    = option;
+        this.isCorrect = isCorrect;
+        this.task      = task;
+    }
+}
